5b4797886f87a752e9c3849c42b35a02658d7454
Armin <armin_mesic@hotmail.de>
2025-03-19 13:45:58 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "subscription_id" {
   description = "Azure Subscription ID"
+  type        = string
 }
 
 variable "tenant_id" {
   description = "The Azure tenant ID."
+  type        = string
 }
 
 variable "app_client_display_name" {
@@ -13,24 +15,29 @@ variable "app_client_display_name" {
 
 variable "admin_password" {
   description = "The administrator password for the VM."
+  type        = string
 }
 
 variable "resource_group" {
   description = "The name of the resource group in which to create the environment."
   default     = "secretsHubPOC"
+  type        = string
 }
 
 variable "poc_name" {
   description = "Name of the POC"
   default     = "secretsHubPOC"
+  type        = string
 }
 
 variable "admin_username" {
   description = "The administrator username for the VM."
   default     = "secretsHubPOCAdmin"
+  type        = string
 }
 
 variable "vm_size" {
   description = "The size of the VM that will be created."
   default     = "Standard_DS1_v2"
+  type        = string
 }
